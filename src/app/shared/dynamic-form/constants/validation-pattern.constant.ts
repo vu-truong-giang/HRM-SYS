c78fea8174972b852b549e0802668888ba5577a2
Vu Truong Giang <vutruonggiangty@gmail.com>
2026-09-09 01:46:24 +0700

    xoa dynamic-form-validator.serrvice

--- src/app/shared/dynamic-form/constants/validation-pattern.constant.ts
+++
@@ -1,7 +0,0 @@
-export const VALIDATION_PATTERN = {
-    EMP_ID: /^EMP\d{3}$/,
-    FULL_NAME: /^[a-zA-ZÀ-ỹ\s]+$/,
-    EMAIL: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-    TEL: /^(0|\+84)(\d{9,10})$/,
-    SALARY: /^\d+(\.\d{1,2})?$/,
-}
